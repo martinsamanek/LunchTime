a874bb9d004d76ab217fadcfa7b426b9dc02d942
Miroslav Kišš <mgr.miroslav.kiss@gmail.com>
2019-10-31 00:00:16 +0100

    Uprava layoutu Uprava stylov a zavedenie less Preformatovanie html

--- LunchTime/wwwroot/js/site.js
+++ LunchTime/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+customElements.define('x-restaurants', class extends HTMLElement { });
+customElements.define('x-restaurant', class extends HTMLElement { });
+customElements.define('x-restaurant-name', class extends HTMLElement { });
+customElements.define('x-restaurant-menuweb', class extends HTMLElement { });
+customElements.define('x-restaurant-menu', class extends HTMLElement { });
+customElements.define('x-restaurant-soups', class extends HTMLElement { });
+customElements.define('x-restaurant-meals', class extends HTMLElement { });
